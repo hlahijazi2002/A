e9863c83843396e9835feed8029c0367ac42c3bb
Hla <hlahijazi22@gmail.com>
2026-06-11 17:13:04 +0300

    Update vite.config.ts
    Update

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,9 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    typescript: false,
-  },
   server: {
     watch: {
       usePolling: true,
